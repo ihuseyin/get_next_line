6e48e1c4df92ef25f665aef4572b3abf4c8c5a33
ihuseyin <borzslam@gmail.com>
2024-11-04 20:38:02 +0300

    get_next_line.c
    Fixed norms and improvement functions.

--- get_next_line.c
+++ get_next_line.c
@@ -1,55 +1,64 @@
 #include "get_next_line.h"
 
-static char	*ft_read_line(int fd, char **remainder)
+static char	*ft_read_line(char **remainder)
 {
-	ssize_t	bytes_read;
-	char	buffer[BUFFER_SIZE + 1];
 	char	*newline;
 	char	*start;
 	char	*line;
 
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read >= 0)
+	if (!*remainder)
+		return (NULL);
+	start = *remainder;
+	newline = ft_strchr(*remainder, '\n');
+	if (newline)
 	{
-		if (bytes_read)
-		{
-			buffer[bytes_read] = '\0';
-			*remainder = ft_strjoin(*remainder, buffer);
-		}
-		start = *remainder;
-		newline = ft_strchr(*remainder, '\n');
-		if (newline)
-		{
-			*newline = '\0';
-			line = malloc(newline - start + 2);
-			if (!line)
-				return (NULL);
-			ft_strncpy(line, start, newline - start);
-			line[newline - start] = '\n';
-			line[newline - start + 1] = '\0';
-			return (line);
-		}
+		*newline = '\0';
+		line = malloc(newline - start + 2);
+		if (!line)
+			return (NULL);
+		ft_strncpy(line, start, newline - start);
+		line[newline - start] = '\n';
+		line[newline - start + 1] = '\0';
+		return (line);
 	}
 	return (NULL);
+}
+
+static void	ft_read_remainder(int fd, char **remainder)
+{
+	ssize_t	bytes_read;
+	char	buffer[BUFFER_SIZE + 1];
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read > 0)
+	{
+		buffer[bytes_read] = '\0';
+		*remainder = ft_strjoin(*remainder, buffer);
+	}
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*remainder;
 	char		*line;
+	char		*new_remainder;
 
 	line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = ft_read_line(fd, &remainder);
-	if (remainder && remainder[0] != '\0')
+	ft_read_remainder(fd, &remainder);
+	line = ft_read_line(&remainder);
+	if (line == NULL && remainder)
 	{
-		remainder = ft_strdup(remainder + ft_strlen(line));
-	}
-	else
-	{
+		line = ft_strdup(remainder);
 		free(remainder);
 		remainder = NULL;
+	}
+	else if (remainder && remainder[0] != '\0')
+	{
+		new_remainder = ft_strdup(remainder + ft_strlen(line));
+		free(remainder);
+		remainder = new_remainder;
 	}
 	return (line);
 }
