e21f7dffaccd0aef0d70159dff5fd4062b3b5c11
ihuseyin <borzslam@gmail.com>
2024-11-16 22:36:19 +0300

    "get_next_line" implementation

---
+++ get_next_line_bonus.c
@@ -0,0 +1,89 @@
+#include "get_next_line_bonus.h"
+
+static char	*ft_read_line(char **remainder)
+{
+	char	*newline;
+	char	*line;
+
+	if (!*remainder)
+		return (NULL);
+	newline = ft_strchr(*remainder, '\n');
+	if (newline != NULL)
+	{
+		line = malloc(newline - *remainder + 2);
+		if (!line)
+			return (NULL);
+		ft_strncpy(line, *remainder, newline - *remainder + 1);
+		return (line);
+	}
+	return (NULL);
+}
+
+static ssize_t	ft_get_remainder(int fd, char **remainder)
+{
+	ssize_t	bytes_read;
+	char	*buffer;
+	char	*new_remainder;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (0);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	buffer[bytes_read] = '\0';
+	if (*remainder)
+	{
+		new_remainder = ft_strjoin(*remainder, buffer);
+		*remainder = new_remainder;
+	}
+	else
+		*remainder = ft_strdup(buffer);
+	free(buffer);
+	return (bytes_read);
+}
+
+static void	ft_return_line(char **remainder, char **line)
+{
+	char	*new_remainder;
+
+	if (*line)
+	{
+		new_remainder = ft_strdup(*remainder + ft_strlen(*line));
+		free(*remainder);
+		*remainder = new_remainder;
+	}
+	else if (*remainder)
+	{
+		*line = ft_strdup(*remainder);
+		free(*remainder);
+		*remainder = NULL;
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*remainder[1024];
+	char		*line;
+	ssize_t		readed_bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	readed_bytes = ft_get_remainder(fd, &remainder[fd]);
+	if (readed_bytes == 0 && (!remainder[fd] || remainder[fd][0] == '\0'))
+	{
+		free(remainder[fd]);
+		return (NULL);
+	}
+	line = ft_read_line(&remainder[fd]);
+	while (!line && readed_bytes > 0)
+	{
+		readed_bytes = ft_get_remainder(fd, &remainder[fd]);
+		line = ft_read_line(&remainder[fd]);
+	}
+	ft_return_line(&remainder[fd], &line);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strncpy(char *dst, const char *src, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *s1, const char *s2);
+
+char	*get_next_line(int fd);
+#endif
